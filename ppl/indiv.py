aeb97a55c42bf8ee2c6f3155fbc9892656d5302d
Jordan Bishop <jordanbishop26@gmail.com>
2021-06-01 10:05:58 +1000

    Add logic for marking elites and only evaling offspring in each gen

--- ppl/indiv.py
+++ ppl/indiv.py
@@ -7,6 +7,7 @@ class Indiv:
         self._inference_strat = inference_strat
         self._fitness = None
         self._time_steps_used = None
+        self._is_elite = False
 
     @property
     def classifiers(self):
@@ -34,6 +35,15 @@ class Indiv:
     @time_steps_used.setter
     def time_steps_used(self, val):
         self._time_steps_used = val
+
+    @property
+    def is_elite(self):
+        return self._is_elite
+
+    @is_elite.setter
+    def is_elite(self, bool_):
+        assert isinstance(bool_, bool)
+        self._is_elite = bool_
 
     def select_action(self, obs):
         """Performs inference on obs using classifiers to predict an action;
--- ppl/ppl.py
+++ ppl/ppl.py
@@ -41,13 +41,17 @@ class PPL:
     def init(self):
         self._pop = init_pop(self._encoding, self._selectable_actions,
                              self._inference_strat)
-        self._eval_pop_fitness(self._pop)
+        self._eval_indivs_fitness(self._pop)
         return self._pop
 
     def run_gen(self):
         new_pop = []
 
         # elitism
+        # first mark everyone as non-elite
+        for indiv in self._pop:
+            indiv.is_elite = False
+        # then do elitist selection
         fitness_desc_sorted_pop = sorted(self._pop,
                                          key=lambda indiv: indiv.fitness,
                                          reverse=True)
@@ -55,13 +59,16 @@ class PPL:
         num_elites = get_hp("num_elites")
         assert (pop_size - num_elites) % 2 == 0
         # make deepcopies of elites because they could possibly be selected as
-        # parents later and mutated, don't want to share refs for that
+        # parents later and modified, don't want to share refs for that
+        elites = []
         for idx in range(0, num_elites):
             elite = copy.deepcopy(fitness_desc_sorted_pop[idx])
-            new_pop.append(elite)
+            elite.is_elite = True
+            elites.append(elite)
 
-        # breeding
+        # breed offspring
         num_breeding_rounds = (pop_size - num_elites) // 2
+        offspring = []
         for _ in range(0, num_breeding_rounds):
             parent_a = tournament_selection(self._pop)
             parent_b = tournament_selection(self._pop)
@@ -69,24 +76,25 @@ class PPL:
                                            self._inference_strat)
             mutate(child_a, self._encoding, self._selectable_actions)
             mutate(child_b, self._encoding, self._selectable_actions)
-            new_pop.append(child_a)
-            new_pop.append(child_b)
-        assert len(new_pop) == pop_size
+            offspring.append(child_a)
+            offspring.append(child_b)
+        self._eval_indivs_fitness(offspring)
 
-        # eval fitness
+        # build new pop
+        new_pop = elites + offspring
+        assert len(new_pop) == pop_size
         self._pop = new_pop
-        self._eval_pop_fitness(self._pop)
         return self._pop
 
-    def _eval_pop_fitness(self, pop):
+    def _eval_indivs_fitness(self, indivs):
         # process parallelism for fitness eval
         num_rollouts = get_hp("num_rollouts")
         gamma = get_hp("gamma")
         with Pool(_NUM_CPUS) as pool:
             results = pool.starmap(self._eval_indiv_fitness,
                                    [(indiv, num_rollouts, gamma)
-                                    for indiv in pop])
-        for (indiv, result) in zip(pop, results):
+                                    for indiv in indivs])
+        for (indiv, result) in zip(indivs, results):
             indiv.fitness = result.perf
             indiv.time_steps_used = result.time_steps_used
 
